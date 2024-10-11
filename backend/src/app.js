b7b7722218e889fb012b54ed0cd07127553bc221
Philipp Sum <sum.philipp@gmail.com>
2024-10-11 16:52:17 +0200

    update
    working minimal example

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const jwt = require('jsonwebtoken')
 
 // passport stuff
 const passport = require('passport');
-const jwtStrategy = require('./strategies/jwt')
+const LocalStrategy = require('passport-local');
 
 // database stuff
 const pool = require('./db/pool')
@@ -21,28 +21,38 @@ const port = process.env.PORT;
 app.use(express.json())
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to the API'
-  });
-});
+passport.use(new LocalStrategy(
+  async (username, password, done) => {
+    const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+    if (!rows[0]) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    
+    const match = await bcrypt.compare(password, rows[0].password);
+    if (match) {
+      return done(null, rows[0]); // Authentication successful
+    } else {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+  }
+));
 
-app.post("/login", (req, res) => {
-    let { email, password } = req.body;
+app.post("/login", async (req, res) => {
+    let { username, password } = req.body;
     //This lookup would normally be done using a database
-    if (email === "paul@nanosoft.co.za") {
-        if (password === "pass") { //the password compare would normally be done using bcrypt.
+    const {rows} = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+    console.log(rows[0]);
+        if (password === rows[0].password) { //the password compare would normally be done using bcrypt.
             const opts = {}
             opts.expiresIn = 120;  //token expires in 2min
-            const secret = "SECRET_KEY" //normally stored in process.env.secret
-            const token = jwt.sign({ email }, secret, opts);
+            const secret = process.env.SECRETKEY; 
+            const token = jwt.sign({ username }, secret, opts);
             return res.status(200).json({
                 message: "Auth Passed",
                 token
             })
         }
-    }
-    return res.status(401).json({ message: "Auth Failed" })
+    return res.status(401).json({message: "Invalid username or password"})
 });
 
 app.get("/protected", verifyToken , (req, res) => {
@@ -51,7 +61,7 @@ app.get("/protected", verifyToken , (req, res) => {
 
 app.get('/', async (req, res) => {
     try {
-        const data = await pool.query(`SELECT * FROM ${databaseName}`)
+        const data = await pool.query(`SELECT * FROM users`)
         res.status(200).send(data.rows)
     } catch (err) {
         console.log(err)
@@ -59,30 +69,28 @@ app.get('/', async (req, res) => {
     }
 })
 
-// // This is for later
-// app.post('/addUser', async (req, res) => {
-//     const {fullname, username, password, membership} = req.body
-//     try {
-//         await pool.query(`INSERT INTO users (fullname, username, password, membership) VALUES ($1, $2, $3, $4)`, [fullname, username, password, membership])
-//         res.status(200).send({ message: "Successfully added child" })
-//     } catch (err) {
-//         console.log(err)
-//         res.sendStatus(500)
-//     }
-// })
-// 
-// 
-// app.get('/setupUser', async (req, res) => {
-//     try {
-//         await pool.query(
-//           `CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, fullname VARCHAR(100), username VARCHAR(100), password VARCHAR(100), memebership VARCHAR(100))`
-//         );
-//         res.status(200).send({ message: "Successfully created table" });
-//     } catch (err) {
-//         console.log(err)
-//         res.sendStatus(500)
-//     }
-// })
+app.post('/addUser', async (req, res) => {
+    const {fullname, username, password, membership} = req.body
+    try {
+        await pool.query(`INSERT INTO users (fullname, username, password, membership) VALUES ($1, $2, $3, $4)`, [fullname, username, password, membership])
+        res.status(200).send({ message: "Successfully added child" })
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
+ app.get('/setupUser', async (req, res) => {
+     try {
+         await pool.query(
+           `CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, fullname VARCHAR(100), username VARCHAR(100), password VARCHAR(100), membership VARCHAR(100))`
+         );
+         res.status(200).send({ message: "Successfully created table" });
+     } catch (err) {
+         console.log(err)
+         res.sendStatus(500)
+     }
+ })
 // 
 // 
 // app.get('/setupPosts', async (req, res) => {
